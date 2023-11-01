94285615dc2923347746f2d186037fa4bb93a311
Moditha Dissanayaka <108186528+Mo-Ditha@users.noreply.github.com>
2023-11-01 23:46:00 +0530

    Create Contact.js

---
+++ Components/Contact/Contact.js
@@ -0,0 +1,14 @@
+import React from "react"
+import HeadTitle from "../../common/HeadTitle/HeadTitle"
+import ContactFrom from "./ContactForm"
+
+const Contact = () => {
+  return (
+    <>
+      <HeadTitle />
+      <ContactFrom />
+    </>
+  )
+}
+
+export default Contact
