8f52f4a11f435f03e59ede7fa42113b3c5d2d6fe
Moditha Dissanayaka <108186528+Mo-Ditha@users.noreply.github.com>
2023-11-09 23:23:10 +0530

    Create Home.js

---
+++ Components/Destinations/Home.js
@@ -0,0 +1,14 @@
+import React from "react"
+import HeadTitle from "../../common/HeadTitle/HeadTitle"
+import AllItem from "./AllItem"
+
+const Destinations = () => {
+  return (
+    <div>
+      <HeadTitle />
+      <AllItem />
+    </div>
+  )
+}
+
+export default Destinations
