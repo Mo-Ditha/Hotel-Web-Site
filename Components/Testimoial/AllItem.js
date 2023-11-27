311d18096671874e2f8a4542f1a3ba56c147600a
Moditha Dissanayaka <108186528+Mo-Ditha@users.noreply.github.com>
2023-11-27 08:13:03 +0530

    Create AllItem.js

---
+++ Components/Testimoial/AllItem.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+//import Card from "./Card"
+// Tdata from "./Tdata"
+
+
+import "./Testimoial.css"
+
+const AllItem = () => {
+  return (
+    <>
+      <section className='Testimonial mtop'>
+        <div className='container grid1'>
+        
+
+
+       
+
+
+
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default AllItem
